f93fed8ae9b295b6be8b0cfedc3d5e8b859fad0c
Deepak Gautam <deepakgautam@Deepaks-MacBook-Pro.local>
2016-08-12 10:48:14 -0700

    Adding Flash Files

--- dat_website/startup_predictor2.py
+++ dat_website/startup_predictor2.py
@@ -3,19 +3,25 @@ from flask import Flask, jsonify, request, render_template
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 import pandas as pd
+from xgboost import XGBClassifier
+import pickle
 
 #---------- MODEL IN MEMORY ----------------#
 
 # Read the scientific data on breast cancer survival,
 # Build a LogisticRegression predictor on it
-# patients = pd.read_csv("haberman.data", header=None)
-# patients.columns=['age','year','nodes','survived']
-# patients=patients.replace(2,0)  # The value 2 means death in 5 years
+# patients = pd.read_csv("CB_final.csv", header=0)
+# patients.columns=['ratingStatus','numberOfRatings','recommendToFriendRating','ceopctapprove']
+# patients = patients.dropna()
 #
-# X = patients[['age','year','nodes']]
-# Y = patients['survived']
-# PREDICTOR = LogisticRegression().fit(X,Y)
+# X = patients.drop('statusID',1)
+# X_ordered = X.reindex_axis(sorted(X.columns), axis=1)
+# Y = patients['statusID']
 
+pkl = open("model_xgb.pkl", "rb")
+#PREDICTOR = LogisticRegression().fit(X,Y)
+PREDICTOR = pickle.load(pkl)#XGBClassifier().fit(X_ordered,Y)
+pkl.close()
 
 #---------- URLS AND WEB PAGES -------------#
 
@@ -39,20 +45,26 @@ def map():
 
 
 # # Get an example and return it's score from the predictor model
-# @app.route("/score/", methods=["POST"])
-# def score():
-#     """
-#     When A POST request with json data is made to this uri,
-#     Read the example from the json, predict probability and
-#     send it with a response
-#     """
-#     # Get decision score for our example that came with the request
-#     data = request.json
-#     x = np.matrix(data["example"])
-#     score = PREDICTOR.predict_proba(x)
-#     # Put the result in a nice dict so we can send it as json
-#     results = {"score": score[0,1]}
-#     return jsonify(results)
+# Get an example and return it's score from the predictor model
+@app.route("/score/", methods=["POST"])
+def score():
+    """
+    When A POST request with json data is made to this uri,
+    Read the example from the json, predict probability and
+    send it with a response
+    """
+    # Get decision score for our example that came with the request
+    data = request.json
+    print(data["example"])
+    x = pd.DataFrame(data["example"],index=['i',])
+    x_ordered = x.reindex_axis(sorted(x.columns), axis=1)
+    score = PREDICTOR.predict_proba(x_ordered)
+    # Put the result in a nice dict so we can send it as json
+
+    results = {"oper":str(score[0,1]), "close":str(score[0,0]), "acq":str(score[0,2]), "ipo":str(score[0,3])}
+    return jsonify(results)
+
+
 
 #--------- RUN WEB APP SERVER ------------#
 
